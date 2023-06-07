8f3a46aa75d60bb98578b6b3a5c750ed94a7c402
Vidisha Patel <vidishapatel@yogis-mbp.lan>
2023-06-07 17:35:14 -0400

    about section

---
+++ src/components/Abouts.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Container, Row, Col, Card, Image } from "react-bootstrap";
+import avatar from "";
+import "../style/About.css";
+
+function About() {
+    return (
+        //About section
+        <section id="about" className="py-5">
+            <h1 className="about-heading">About Me</h1>
+            <Container>
+                <Row>
+                    <Col 
+                    xs={12}
+                    md={4}
+                    className="d-flex justify-content-center align-items-cente mb-md-0">
+                        <Image src={avatar} rounded fluid className="about-image" />
+                    </Col>
+                    <Col xs={12} md={8}>
+                        <Card>
+                            <Card.Body>
+                            <Card.Title className="text-center mb-4">
+                                About Me
+                            </Card.Title>
+                            <Card.Text>
+                                Write something about me
+                            </Card.Text>
+                            <Card.Text>
+                                write something about me
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+}
+
+//export about
+export default About;
