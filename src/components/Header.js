237ca88ee0bb978a4aa67499fc9841220626ab81
Vidisha Patel <vidishapatel@yogis-mbp.lan>
2023-06-12 18:34:23 -0400

    importing files for header

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navigation from "./Navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChessKing } from "@fortawesome/free-solid-svg-icons";
+import { Container, Row, Col } from "react-bootstrap";
+import "../style/Header.css";
