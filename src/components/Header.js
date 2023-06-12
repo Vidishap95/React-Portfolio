be59bd504db39b6e1d6b329497721a5c74b3f6da
Vidisha Patel <vidishapatel@yogis-mbp.lan>
2023-06-12 19:00:10 -0400

    bootstrap added for navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ function Header () {
         <Container fluid className="bg-dark">
             <Row className="justify-conent-center text-center mb-0">
                 <Col className="="pb-0>
-                    <h1 className="text-white" style={{ fontSize="70px"}}>
+                    <h1 className="text-white" style={{ fontSize:"70px"}}>
                         <FontAwesomeIcon
                         icon={faChessKnight}
                         className="fa-knight"
