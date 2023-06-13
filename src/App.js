d98a86f2a7890d30a5746d58ad68fcd6f4dc4142
Vidisha Patel <vidishapatel@yogis-mbp.lan>
2023-06-12 22:15:45 -0400

    adding style to app.

--- src/App.js
+++ src/App.js
@@ -8,4 +8,34 @@ import Resume from "./components/Resume";
 import Footer from "./components/Footer";
 
 
-// 
+// Import bootstrap and custome css styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style/App.css";
+
+//defining the app components
+function App () {
+    return (
+        <div className="App">
+            <Router>
+                <Header />
+                {/* <Navigation /> */}
+                 {/* <Header/> */}
+                <div className="container mt-5">
+                    <Routes>
+                        <Route path="/Vidishap95/React-Portfolio/" element={<About />} />
+                        {/* <Route path="/Vidishap95/React-Portfolio/home" element={<Home />} /> */}
+                        <Route path="/Vidishap95/React-Portfolio/about" element={<About />} /> 
+                        <Route path="/Vidishap95/React-Portfolio/portfolio" element={<Portfolio />} /> 
+                        <Route path="/Vidishap95/React-Portfolio/contact" element={<Contact />} /> 
+                        <Route path="/Vidishap95/React-Portfolio/resume" element={<Resume />} /> 
+
+                    </Routes>
+                </div>
+                <Footer/>
+            </Router>
+        </div>
+    );
+}
+
+//render footer components
+export default App;
