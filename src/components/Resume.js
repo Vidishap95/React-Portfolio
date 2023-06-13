b77a04f6a429409002a4af471f4362705d9603d8
Vidisha Patel <vidishapatel@yogis-mbp.lan>
2023-06-12 21:54:13 -0400

    components for resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,4 +3,24 @@ import { Container, Row, Col, Button  } from "react-bootstarp";
 import "../style/Resume.css";
 import PDF from "../documentation/VidishaPatelResume.pdf";
 
-//
+//resume component
+function Resume () {
+    return (
+        <section id="resume" className="resume-margin">
+      <Container className="resume-section">
+        <Row>
+          <Col className="text-center">
+            <h1>Resume</h1>
+            <hr className="star-primary" />
+            <Button variant="dark" href={PDF} download>
+              Download Resume
+            </Button>
+          </Col>
+          </Row>
+          </Container>
+        </section>
+    );
+}
+
+//export resume
+export default Resume;
