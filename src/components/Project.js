3800e6b76f7636cd168ecc1bfad5f0ecb266a9f2
Vidisha Patel <vidishapatel@yogis-mbp.lan>
2023-06-12 21:44:31 -0400

    project logic

---
+++ src/components/Project.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../style/Project.css";
+
+//project component
+function Project (props) {
+    return (
+        //card container for each project
+        <div className="card">
+            <h5 className="card-title">{props.title}</h5>
+            <a href={props.deployedLink} target="_blank" rel="noreferrer">
+            </a>
+            <div className="card-body">
+                <p className="card-text">{props.description}</p>
+                <a
+                href={props.githubLink}
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-repo">
+                </a>
+            </div>
+        </div>
+    );
+}
+
+//export project
+export default Project
